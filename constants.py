a3c3704a041c616431c1e8ecd443ebcf217ad6b1
Xewus <xewuss@yandex.ru>
2022-04-20 01:00:49 +0300

    v.0.1.5 .env

--- constants.py
+++ constants.py
@@ -1,8 +1,11 @@
 HH_LOGIN_URL = 'https://spb.hh.ru/account/login?backurl=%2F'
-RESUME_URL = 'https://spb.hh.ru/resume/e7b22514ff0971ecc00039ed1f6e3968595254'
 
 PHONE_INPUT_XPATH = '//input[@placeholder="Email или телефон"]'
 PASSWWORD_XPATH = '//button[@data-qa="expand-login-by-password"]'
 PASSWWORD_INPUT_XPATH = '//input[@placeholder="Пароль"]'
 SUBMIT_BUTTON_XPATH = '//button[@data-qa="account-login-submit"]'
-REFRESH_BUTTON_XPATH = '//button[@data-qa="resume-update-message"]'
+REFRESH_BUTTON_XPATH = '//*[@id="HH-React-Root"]/div/div/div[1]/div/div/div/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div[4]/div/button'
+
+'/html/body/div[8]/div/div/div[1]/div/div/div/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div[4]/div/button'
+'//*[@id="HH-React-Root"]/div/div/div[1]/div/div/div/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div[4]/div/button'
+'//*[@id="HH-React-Root"]/div/div/div[1]/div/div/div/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div[4]/div'
--- hh.py
+++ hh.py
@@ -1,5 +1,7 @@
 from time import sleep
 
+
+from decouple import config
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,8 +10,9 @@ import constants as const
 import utils
 
 
-PHONE = ''
-PASSWORD = ''
+PHONE = config('PHONE', default='')
+PASSWORD = config('PASSWORD', default='')
+RESUME_URL = config('RESUME_URL', default='')
 
 
 def main(browser):
@@ -22,10 +25,11 @@ def main(browser):
     utils.input_key(browser, const.PASSWWORD_INPUT_XPATH, PASSWORD)
     utils.click_button(browser,  const.SUBMIT_BUTTON_XPATH)
 
-    browser.get(const.RESUME_URL)
+    browser.get(RESUME_URL)
     sleep(utils.PAUSE)
 
     utils.click_button(browser, const.REFRESH_BUTTON_XPATH)
+    browser.save_screenshot('resume')
     sleep(9)
 
     browser.quit()
