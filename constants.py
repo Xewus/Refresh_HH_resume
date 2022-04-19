3ecf6a47568c2dd3db1e84e8b70c8002349e7631
Xewus <xewuss@yandex.ru>
2022-04-19 22:26:27 +0300

    v.0.1.2 consts

---
+++ constants.py
@@ -0,0 +1,8 @@
+HH_LOGIN_URL = 'https://spb.hh.ru/account/login?backurl=%2F'
+RESUME_URL = 'https://spb.hh.ru/resume/e7b22514ff0971ecc00039ed1f6e3968595254'
+
+PHONE_INPUT_XPATH = '//input[@placeholder="Email или телефон"]'
+PASSWWORD_XPATH = '//button[@data-qa="expand-login-by-password"]'
+PASSWWORD_INPUT_XPATH = '//input[@placeholder="Пароль"]'
+SUBMIT_BUTTON_XPATH = '//button[@data-qa="account-login-submit"]'
+REFRESH_BUTTON_XPATH = '//button[@data-qa="resume-update-message"]'
--- hh.py
+++ hh.py
@@ -5,12 +5,11 @@ from selenium import webdriver
 # Импортируем классы для Chrome. Если у вас другой браузер - измените импорт.
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
+import constants as const
 
-HH_LOGIN_URL = 'https://spb.hh.ru/account/login?backurl=%2F'
-RESUME_URL = 'https://spb.hh.ru/resume/e7b22514ff0971ecc00039ed1f6e3968595254'
+
 USERNAME = ''
 PASSWORD = ''
 PAUSE = randint(1, 4)
@@ -18,30 +17,30 @@ PAUSE = randint(1, 4)
 service = Service(executable_path=ChromeDriverManager().install())
 
 browser = webdriver.Chrome(service=service)
-browser.get(HH_LOGIN_URL)
+browser.get(const.HH_LOGIN_URL)
 browser.maximize_window()
 sleep(PAUSE)
 
-login_input = browser.find_element(By.XPATH, '//input[@placeholder="Email или телефон"]')
-login_input.send_keys(USERNAME)
-sleep(3)
+phone_input = browser.find_element(By.XPATH, const.PHONE_INPUT_XPATH)
+phone_input.send_keys(USERNAME)
+sleep(PAUSE)
 
-goto_password = browser.find_element(By.XPATH, '//button[@data-qa="expand-login-by-password"]')
+goto_password = browser.find_element(By.XPATH, const.PASSWWORD_XPATH)
 goto_password.click()
 sleep(PAUSE)
 
-password_input = browser.find_element(By.XPATH, '//input[@placeholder="Пароль"]')
+password_input = browser.find_element(By.XPATH, const.PASSWWORD_INPUT_XPATH)
 password_input.send_keys(PASSWORD)
 sleep(PAUSE)
 
-submit_button = browser.find_element(By.XPATH, '//button[@data-qa="account-login-submit"]')
+submit_button = browser.find_element(By.XPATH, const.SUBMIT_BUTTON_XPATH)
 submit_button.click()
 sleep(PAUSE)
 
-browser.get(RESUME_URL)
+browser.get(const.RESUME_URL)
 sleep(PAUSE)
 
-refresh = browser.find_element(By.XPATH, '//button[@data-qa="resume-update-message"]')
+refresh = browser.find_element(By.XPATH, const.REFRESH_BUTTON_XPATH)
 refresh.click()
 sleep(9)
 
